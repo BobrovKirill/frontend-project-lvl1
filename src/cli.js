d759bcd530ab0dc17a19d5b8991e3fff756c3e51
bobrovkirill <bobrovkirill11@gmail.com>
2020-05-10 18:39:33 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line import/no-unresolved
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line camelcase
 const user_name = () => {
-    let request_name = readlineSync.question('May I have your name? ');
-    let request = console.log ('Hello, ' + request_name + '!');
-    return request;
-}
+  // eslint-disable-next-line camelcase
+  const request_name = readlineSync.question('May I have your name? ');
+  // eslint-disable-next-line camelcase
+  const request = console.log(`Hello, ${request_name}!`);
+  return request;
+};
+// eslint-disable-next-line camelcase
 export default user_name;
-1
